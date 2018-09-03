5c0f2a70ec833e7df4fb388ac22ed150a7671b17
ritenojb <ritenojb@rose-hulman.edu>
2018-09-03 11:54:06 -0400

    DONE

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -43,7 +43,7 @@ num2 = 4 * math.cos(2)
 print(num1 + num2)
 
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
@@ -51,10 +51,10 @@ print(num1 + num2)
 #   Print both of the expressions that you write.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
-
+print(math.sqrt(2))
+print(2**0.5)
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number")
@@ -93,3 +93,16 @@ print(num1 + num2)
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
+print(type(3.14))
+print(type("hello"))
+print(type('hello'))
+print(type("a b c"))
+print(type(3 + 3))
+print(type("3" + "3"))
+print(type(2 ** 100))
+print(type(2.0 * 100))
+print(type(math.sin(8)))
+print(type(math.sin))
+print(type(print))
+print(type(math))
+print(type('math'))
